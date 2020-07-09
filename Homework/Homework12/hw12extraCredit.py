a2c2a03cc404a0aa60cdc2647acda234a7cf9f1f
aguilaraul <raula@live.com>
2020-07-09 10:18:56 -0700

    Homework:Homework 12
    Homework 12 Extra Credit complete

--- Homework/Homework12/hw12extraCredit.py
+++ Homework/Homework12/hw12extraCredit.py
@@ -3,11 +3,17 @@
 # date      July 9, 2020
 #
 # CS 138 1535 Homework 12 Extra Credit
+# Base-10 to any other base converter, but not back to base 10.
 # Write a program that allows a user to enter a number and a base and
 # then prints out the digits of the number in the new base.
 #
 # Alogrithm:
-#
+# 1. Take number and base from user
+# 2. Append remainder of num%base into list
+# 3. Recursively run conversion method using num//base until quotient is
+#    zero
+# 4. Reverse the list, return the list
+# 
 
 
 def baseConversion(num, base):
@@ -26,9 +32,24 @@ def conversion(num, base, result):
 
 
 def main():
-    # @Debug
-    # Take user input for num and base
-    # but validate answers first for int
-    print(baseConversion(1234, 20))
+    validNum = False
+    validBase = False
+
+    while not validNum:
+        try:
+            num = int(input("Enter number to convert: "))
+            validNum = True
+        except:
+            print("Number is not valid. Re-enter.\n")
+
+    while not validBase:
+        try:
+            base = int(input("Enter base to convert to: "))
+            validBase = True
+        except:
+            print("Base is not valid. Re-enter.\n")
+
+    print(baseConversion(num, base))
+
 
 main()
