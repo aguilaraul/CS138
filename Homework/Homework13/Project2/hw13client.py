97384c787efcd7fc86a73915ac4625b6ba0694ac
Raul Aguilar <raula@live.com>
2020-07-19 22:01:28 -0700

    Homework:Homework 13 Homework 13 complete.

---
+++ Homework/Homework13/Project2/hw13client.py
@@ -0,0 +1,34 @@
+# echo_client.py
+# https://www.bogotobogo.com/python/python_network_programming_server_client.php
+#
+# author    Raul Aguilar
+# date      July 19, 2020
+#
+# CS 138 Homework 13 Project 2: Server
+# Modify the server to send back the string in reverse order.
+#
+# Algorithm:
+# 1. Get server info
+# 2. Create client socket and connect to server
+# 3. Get user input
+# 4. Decode string into bytes and send data to server
+# 5. Get data (reversed string) from server
+# 6. Close the connection and display the data received
+#
+import socket
+
+# Server info
+host = '127.0.0.1'
+port = 12345
+
+# connect to server
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((host, port))
+
+# get user string
+message = input("Send a sentence to the server: ")
+s.sendall(message.encode('UTF-8'))
+
+data = s.recv(1024)
+s.close()
+print('Received', repr(data))
---
+++ Homework/Homework13/Project2/hw13server.py
@@ -0,0 +1,38 @@
+# echo_server.py
+# https://www.bogotobogo.com/python/python_network_programming_server_client.php
+#
+# author    Raul Aguilar
+# date      July 19, 2020
+#
+# CS 138 Homework 13 Project 2: Server
+# Modify the server to send back the string in reverse order.
+#
+# Algorithm:
+# 1. Create address and port
+# 2. Create (server) socket and assign address and port to it
+# 3. Listen for connection from client. Store new client socket and
+# client address (hostaddr, port). Display connected message
+# 4. While there is data (sentence) to recieve, store data, reverse it,
+# send it back to client, display data that was recieved
+# 5. Close the connection
+#
+import socket
+
+host = '127.0.0.1'
+port = 12345
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+
+s.listen(1)
+conn, addr = s.accept()
+print('Connected by', addr)
+while True:
+    data = conn.recv(1024)
+    reverseMessage = data[::-1]
+    if not data:
+        break
+    conn.sendall(reverseMessage)
+    print('Received', repr(data))
+
+conn.close()
