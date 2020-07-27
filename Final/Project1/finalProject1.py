1c37e857a4d3224b56bc6cf197bea22ff6862c8e
Raul Aguilar <raula@live.com>
2020-07-27 15:29:47 -0700

    Working Separate Class
    Seperated the main work into its own spellchecker class. Saving this before I implement the GUI and mess everything up

--- Final/Project1/finalProject1.py
+++ Final/Project1/finalProject1.py
@@ -1,28 +1,23 @@
 #
 # author    Raul Aguilar
-# date      July 26, 2020
+# date      July 27, 2020
 #
 # CS 138 1535 Final Project 1
 #
 #
-from string import punctuation
-from interface import Interface
+#
 from spellchecker import SpellChecker
 
 def main():
-    Interface()
-    SpellChecker()
-
-    #dictionary = SpellChecker.populateDictionary("english.txt")
+    sc = SpellChecker("english.txt")
     book = open("taleoftwocities.txt", "r", encoding='utf-8')
 
     for line in book:
         if line.strip() != "":
             line_words = line.split()
             for word in line_words:
-                word = word.lower()
-                if SpellChecker.spellCheck(word) != True:
-                    print(spellCheck(word))
+                if sc.spellCheck(word) != True:
+                    print(sc.spellCheck(word))
 
 
 main()
--- Final/Project1/spellchecker.py
+++ Final/Project1/spellchecker.py
@@ -1,7 +1,24 @@
-class SpellChecker:
-    def __init_(self, filename):
-        self.dictionary = populateDictionary(filename)
-    
+# spellchecker.py
+#
+# author    Raul Aguilar
+# date      July 27, 2020
+#
+# CS 138 1535 Final Project 1
+#
+#
+from string import punctuation
+
+class SpellChecker():
+    def __init__(self, filename):
+        self.dictionary = self.populateDictionary(filename)
+
+    def populateDictionary(self, filename):
+        dictionary = []
+        words = open(filename, "r")
+        for word in words:
+            dictionary.append(word.strip())
+        return dictionary
+
     def binarySearch(self, x, L):
         low = 0
         high = len(L)-1
@@ -18,25 +35,16 @@ class SpellChecker:
 
         return -1
 
-
-    def populateDictionary(self, filename):
-        words = []
-        dictionary = open(filename, "r")
-        for word in dictionary:
-            words.append(word.strip())
-        return words
-
-
     def spellCheck(self, word):
         if len(word) == 0:
             return True
 
         if word[0] in punctuation:
-            return spellCheck(word[1:], self.dictionary)
+            return self.spellCheck(word[1:])
         if word[-1] in punctuation:
-            return spellCheck(word[:-1], self.dictionary)
+            return self.spellCheck(word[:-1])
 
-        if binarySearch(word, dictionary) != -1:
+        if self.binarySearch(word, self.dictionary) != -1:
             return True
         else:
             return word
