5b05e15788dc423e3facaa058bd57cec692debe9
Raul Aguilar <raula@live.com>
2020-08-05 00:28:37 -0700

    Update interface.py

--- Final/Project3/interface.py
+++ Final/Project3/interface.py
@@ -43,10 +43,13 @@ class Interface:
                     return b.getLabel()
 
     # Screens
-    def undrawButtons(self):
-        for b in self.buttons:
-            b.undraw()
+    def undrawMenu(self):
+        for b in self.buttons: b.undraw()
         self.buttons.clear()
+        for e in self.entries: e.undraw()
+        self.entries.clear()
+        for t in self.texts: t.undraw()
+        self.texts.clear()
 
     def drawExitBackButtons(self):
         b = Button(self.win, Point((WINW / 6)*4, WINH - WINH / 16), 75, 30, "Back")
@@ -68,7 +71,7 @@ class Interface:
         buttons.append(b)
 
     def mainMenu(self):
-        self.undrawButtons()
+        self.undrawMenu()
         self.drawMainMenu()
         while True:
             ans = self.choose(["Add Employee", "Remove Employee", "Save Database", "Load Database", "Exit"])
@@ -87,21 +90,50 @@ class Interface:
         self.drawExitBackButtons()
 
     def addEmployee(self):
-        self.undrawButtons()
+        self.undrawMenu()
         self.drawAddEmployee()
         while True:
             ans = self.choose(["Back", "Exit", "Parttime Employee", "Salary Employee", "Hourly Employee"])
             return ans
 
     def drawTypeEmployee(self, choice):
-        self.undrawButtons() # undraw previous menu
+        self.undrawMenu() # undraw previous menu
+        entries = self.entries
+        texts = self.texts
+        
         # Draw buttons
         b = Button(self.win, Point(WINW/5, WINH-WINH/16), 150, 30, "Add Employee")
         self.buttons.append(b)
         self.drawExitBackButtons()
 
+        t = Text(Point(WINW/3, (WINH/16)*3), "First Name:")
+        texts.append(t)
+        e = Entry(Point((WINW/3)*2, (WINH/16)*3), 20)
+        entries.append(e)
+        t = Text(Point(WINW/3, (WINH/16)*4), "Last Name:")
+        texts.append(t)
+        e = Entry(Point((WINW/3)*2, (WINH/16)*4), 20)
+        entries.append(e)
+        t = Text(Point(WINW/3, (WINH/16)*5), "ID Number:")
+        texts.append(t)
+        e = Entry(Point((WINW/3)*2, (WINH/16)*5), 20)
+        entries.append(e)
+
         # Draw Entries
-        
+        if choice == "Parttime Employee":
+            t = Text(Point(WINW/3, (WINH/16)*6), "Amount per class: $")
+            texts.append(t)
+            e = Entry(Point((WINW/3)*2, (WINH/16)*6), 20)
+            entries.append(e)
+            t = Text(Point(WINW/3, (WINH/16)*7), "Number of classes teaching:")
+            texts.append(t)
+            e = Entry(Point((WINW/3)*2, (WINH/16)*7), 20)
+            entries.append(e)
+        elif choice == "Salary Employee":
+            e = Entry
+
+        for e in entries: e.draw(self.win)
+        for t in texts: t.draw(self.win)
 
     def addTypeEmployee(self, choice):
         self.drawTypeEmployee(choice)
